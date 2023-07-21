07da47cc3be8d8d8f84d2681e64f67696f95121f
Altay Karakus <altay.karakus@piworks.net>
2023-07-22 01:40:56 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function ClassA() {
+  const a = 5;
+  const str = "hello".replaceAll("l", "L");
+  console.log(a, str);
+  const test = [1, 2, 3, 4, 5];
+  const ary = test.map((t) => t * 2);
+  console.log(ary);
+}
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                { targets: "defaults", useBuiltIns: "usage", corejs: 3 },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
